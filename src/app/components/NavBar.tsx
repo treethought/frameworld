3b98bc00214b877755ec6f2eb4756ff85505c487
Cam Sweeney <cpsweene@gmail.com>
2024-02-27 19:55:39 -0800

    navbar pages

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,97 +2,165 @@
 import { useApp } from "@/context/AppContext";
 import ProfileAvatar from "./ProfileAvatar";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
+  const pathname = usePathname();
   const { user } = useApp();
+  const pages = ["frames", "feed"];
+  const loginRequired: string[] = [];
+
+  const isCurrentPage = (page: string) => {
+    return pathname.replace(/^\/|\/$/g, "") === page;
+  };
   return (
-    <div className="navbar bg-base-100 text-primary border-b-2 border-primary">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div>
+      <div className="navbar bg-base-100 text-primary border-b-2 border-primary">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost lg:hidden text-base-content"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
+              <FontAwesomeIcon icon={faBars} />
+            </label>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content z-10 bg-base-200 mt-3 p-2 shadow rounded-box w-52"
+            >
+              {pages.map((page) => (
+                <li key={page}>
+                  <Link href={`/${page.toLowerCase()}`} passHref>
+                    <span
+                      className={`px-4 py-2 rounded capitalize ${isCurrentPage(page)
+                          ? "text-primary-600 border border-primary-600"
+                          : "text-base-content hover:text-secondary-focus"
+                        }`}
+                    >
+                      {page}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+
+              {user &&
+                loginRequired.map((page) => (
+                  <li key={page}>
+                    <Link href={`/${page.toLowerCase()}`} passHref>
+                      <span
+                        className={`px-4 py-2 rounded capitalize ${isCurrentPage(page)
+                            ? "text-primary-600 border border-primary-600"
+                            : "text-base-content hover:text-secondary-focus"
+                          }`}
+                      >
+                        {page}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="navbar-center">
-        <Link href="/" className="btn btn-ghost text-xl">frameworld</Link>
-      </div>
-      <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <ThemeSelect />
-        <div className="flex items-center p-4">
-          <span className="px-2">
-            {user?.display_name}
-          </span>
-          {user?.pfp_url
-            ? <ProfileAvatar user={user} className="w-10" />
-            : null}
+
+          <div className="flex-row items-center hidden md:flex gap-2">
+            <Link href="/" passHref>
+              <span className="font-manrope font-bold text-xl tracking-wider lowercase ">
+                frameworld
+              </span>
+            </Link>
+          </div>
         </div>
 
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item">
-            </span>
-          </div>
-        </button>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {pages.map((page) => (
+              <li key={page}>
+                <Link href={`/${page.toLowerCase()}`} passHref>
+                  <span
+                    className={`px-4 py-2 rounded capitalize ${isCurrentPage(page)
+                        ? "text-primary-600 border border-primary-600"
+                        : "text-base-content hover:text-secondary-focus"
+                      }`}
+                  >
+                    {page}
+                  </span>
+                </Link>
+              </li>
+            ))}
+
+            {user &&
+              loginRequired.map((page) => (
+                <li key={page}>
+                  <Link href={`/${page.toLowerCase()}`} passHref>
+                    <span
+                      className={`px-4 py-2 rounded capitalize ${isCurrentPage(page)
+                          ? "text-primary-600 border border-primary-600"
+                          : "text-base-content hover:text-secondary-focus"
+                        }`}
+                    >
+                      {page}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+
+        <div className="navbar-end">
+          <SearchButton />
+          {user?.pfp_url && <ProfileAvatar user={user} className="w-10" link />}
+          <NotificationIndicator />
+        </div>
       </div>
     </div>
+  );
+}
+
+function SearchButton() {
+  return (
+    <button className="btn btn-ghost btn-circle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </button>
+  );
+}
+
+function NotificationIndicator() {
+  return (
+    <button className="btn btn-ghost btn-circle">
+      <div className="indicator">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+          />
+        </svg>
+        <span className="badge badge-xs badge-primary indicator-item">
+        </span>
+      </div>
+    </button>
   );
 }
 
